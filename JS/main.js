b372b4521e738b52f407c39fb7105fae7a125f1b
862851 <47528781+862851@users.noreply.github.com>
2019-06-17 08:30:10 -0400

    made code work

--- JS/main.js
+++ JS/main.js
@@ -8,11 +8,14 @@ function largerInteger(){
 	var number1 = parseInt(prompt("Enter a number"));
 	var number2 = parseInt(prompt("Enter another number"));
 	if (number1 > number2) {
-		document.getByElementID("Biggie").innerHTML = "The first number: " + number1
-	} else if (number2 > number1){
-		document.getElementByElementID("Biggie").innerHTML = "The second number: " + number2
+		document.getElementById("biggie").innerHTML = "The first number: " + number1
+		alert(number1)
+	} else if (number2 > number1) {
+		document.getElementById("biggie").innerHTML = "The second number: " + number2
+		alert(number2)
 	} else {
-		document.getByElementID("Biggie").innerHTML = "Oki doki something brokie"
+		document.getElementById("biggie").innerHTML = "Oki doki something brokie"
+		alert("Ya broke it")
 	}
 }
 
@@ -23,13 +26,13 @@ function positiveNegative(){
 	var number3 = parseInt(prompt("Enter a third number"));
 	var x = number1 + number2 + number3;
 	if (x < 0){
-		document.getByElementID("Positivity").innerHTML = "The product is negative"
+		document.getByElementId("positivity").innerHTML = "The product is negative"
 	} else if (x > 0){
-		document.getByElementID("Positivity").innerHTML = "The product is positive"
+		document.getByElementId("positivity").innerHTML = "The product is positive"
 	} else if (x = 0){
-		document.getByElementID("Positivity").innerHTML = "Bro, that's just 0. Neither positive nor negative"
+		document.getByElementId("positivity").innerHTML = "Bro, that's just 0. Neither positive nor negative"
 	} else{
-		document.getByElementID("Positivity").innerHTML = "Oki doki something brokie"
+		document.getByElementId("positivity").innerHTML = "Oki doki something brokie"
 	}
 }
 
@@ -40,42 +43,42 @@ function orderedNumbers(){
 	var no3 = parseInt(prompt("Enter a third number"));
 	if (no1 > no2 && no1 > no3){
 		if (no2 > no3){
-			document.getByElementID("LawAndOrder").innerHTML = no1 + ", " + no2 + ", " + no3
+			document.getByElementId("LawAndOrder").innerHTML = no1 + ", " + no2 + ", " + no3
 		} else if (no3 > no2){
-			document.getByElementID("LawAndOrder").innerHTML = no1 + ", " + no3 + ", " + no2
+			document.getByElementId("LawAndOrder").innerHTML = no1 + ", " + no3 + ", " + no2
 		} else {
-			document.getByElementID("LawAndOrder").innerHTML = "Oki doki something brokie"
+			document.getByElementId("LawAndOrder").innerHTML = "Oki doki something brokie"
 		}
 	} else if (no2 > no1 && no2 > no3){
 		if (no1 > no3){
-			document.getByElementID("LawAndOrder").innerHTML = no2 + ", " + no1 + ", " + no3
+			document.getByElementId("LawAndOrder").innerHTML = no2 + ", " + no1 + ", " + no3
 		} else if (no3 > no1){
-			document.getByElementID("LawAndOrder").innerHTML = no2 + ", " + no3 + ", " + no1
+			document.getByElementId("LawAndOrder").innerHTML = no2 + ", " + no3 + ", " + no1
 		} else {
-			document.getByElementID("LawAndOrder").innerHTML = "Oki doki something brokie"
+			document.getByElementId("LawAndOrder").innerHTML = "Oki doki something brokie"
 		}
 	} else if (no3 > no1 && no3 > no2){
 		if (no1 > no2){
-			document.getByElementID("LawAndOrder").innerHTML = no3 + ", " + no1 + ", " + no2
+			document.getByElementId("LawAndOrder").innerHTML = no3 + ", " + no1 + ", " + no2
 		} else if (no2 > no1){
-			document.getByElementID("LawAndOrder").innerHTML = no3 + ", " + no2 + ", " + no1
+			document.getByElementId("LawAndOrder").innerHTML = no3 + ", " + no2 + ", " + no1
 		} else {
-			document.getByElementID("LawAndOrder").innerHTML = "Oki doki something brokie"
+			document.getByElementId("LawAndOrder").innerHTML = "Oki doki something brokie"
 		}
 	}
 	 else {
-		document.getByElementID("LawAndOrder").innerHTML = "Oki doki something brokie"
+		document.getByElementId("LawAndOrder").innerHTML = "Oki doki something brokie"
 	}
 }
 
 
 function namePlease(userName){ //NOT PART OF WEBSITE
-	if (userName = "Tidalwave001"){
+	if (userName = "TIdalwave001"){
 		console.log("Hello " + userName);
 	} else if (userName = "Joe"){
 		console.log("Welcome " + userName);
 	} else {
-		console.log("Invalid username");
+		console.log("InvalId username");
 	}
 }
 
@@ -106,39 +109,49 @@ for (i = 1; i < 101; i++)
 	countingLoop(i);
 
 function guessNumber(){
-	var guess = parseInt(prompt("I'm thinking of a number between 1 and 100"));
-	var rNg = Math.floor(Math.random()*100);
-	if (guess != rNg) {
-		prompt("Try again boi")
-	} else if (guess = rNg) {
-		alert("Ayy lmao, you got it!");
-	} else {
-		alert("Oki doki something brokie")
+	function rnG (){
+		return Math.floor(Math.random() * Math.floor(101));
 	}
+	console.log("Answer: " + rnG())
+	var rNg = rnG()
+	var guess = parseInt(prompt("I'm thinking of a number between 1 and 100"));
+	while (guess !== rNg){
+		var guess = parseInt(prompt("Nope, try again"))
+	if (guess == rNg) {
+		break;
+	}
+	}
+	alert("Ayy lmao, you got it!")
 }
 
 
 
 
 function markiBoi(){
-var marksList = [];
+var marksList = [90];
 var listLength = marksList.length;
 	while (uhMarks = "Y"){
-    var addMarks = parseInt(prompt("What's the mark you would like to add?"));
+   	var addMarks = parseInt(prompt("What's the mark you would like to add?"));
     marksList.push(addMarks);
     var uhMarks  = prompt("Would you like to add to the marks list?[Y/N]").toLocaleUpperCase();
     if (uhMarks !== "Y")  {
     	break;
-     }
-    }
+		}
+		function getSum (total, num){
+			return total + num
+		}
+		var bigMark = marksList.reduce(getSum)
+		var marksAverage = bigMark / listLength
+    	for (i=0; i<listLength; i++){
+    	document.getElementById("AverageJoe").innerHTML = "Your grades are: " + marksList[i] + ", "
+  		document.getElementById("AverageJoey").innerHTML = "You got a " + marksList[i] + " on a test!"
+  		document.getElementById("AverageJoestar").innerHTML = "Your average is " + marksAverage
+		}
+	}
+}
     //Doesn't work from here
-    var marksAverage = 0;
-  	marksAverage = (marksAverage + marksList[i])/marksList.length;
-    for (i=0; i<listLength; i++);
-    document.getElementById("AverageJoe").innerHTML = "Your grades are: " + marksList[i] + ", "
-  	document.getElementById("AverageJoey").innerHTML = "You got a " + marksList[i] + " on a test!"
-  	document.getElementById("AverageJoestar").innerHTML = "Your average is " + marksAverage
-  	}
+
+
 
 
 
@@ -147,7 +160,7 @@ var listLength = marksList.length;
 function lengthReader() {
 	var string = prompt("What text would you like us to measure?")
 	var stringLength = string.length
-	document.getElementById("longBoi").innerHTML = "The length of the following text is " + stringLength + "letters. " + string	
+	document.getElementById("longBoi").innerHTML = "The length of the following text is " + stringLength + " letters: " + string	
 }
 
 function mockingSpongebob() {
@@ -172,7 +185,7 @@ function signIn() {
 	if (igninput==ign){
 		var passphraseinput = prompt("What's the password?").toLocaleUpperCase();
 		if (passphraseinput==passphrase) {
-			document.getByElementID("gucci").innerHTML = "Welcome to the rice fields!"
+			document.getByElementId("gucci").innerHTML = "Welcome to the rice fields!"
 		} else {
 			alert ("You're WRONG!");
 		}
@@ -183,7 +196,7 @@ function signIn() {
 }
 
 function pigLatin () {
-	//Not tested
+	//Does not work
 	var text = prompt("What would you like to encrypt in Pig Latin?")
 	var tobetranslated = text.split(" ", text.length)
 	for (var i=0; i < tobetranslated.length; i++)
@@ -206,7 +219,7 @@ function encryptionThingy () {
 	var passphrase = "PRODUCTIVE";
 	var passphraseinput = prompt("Password?").toLocaleUpperCase();
 	if (passphraseinput = passphrase) {
-		var text = prompt("What would you like to hide?");
+		var text = prompt("What would you like to hIde?");
 		var ugh = ""
 		var length = text.length
 		for (var i=0; i<length; i++){
